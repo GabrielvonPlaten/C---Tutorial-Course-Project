9388e484efc96d081fafb7026b33bd319b28f249
Gabriel von Platen <gabrielvonplaten@gmail.com>
2019-06-12 20:26:18 +0200

    Console Writes. First lines of code of C#

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World
+{
+    class Program
+    {
+        // Starting point of my program
+        static void Main(string[] args)
+        {
+            // Console.ForegroundColor = ConsoleColor.Yellow; // Colors must be on top
+            // Console.BackgroundColor = ConsoleColor.DarkBlue;
+            // Console.Clear();
+            Console.WriteLine("Hello, world");
+            Console.WriteLine("Hello Gabriel von Platen!");
+            Console.Read(); // The console is kept open until Enter is pressed
+        }
+    }
+}
