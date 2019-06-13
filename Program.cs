2c60595a1b99c2b837fe9d2fbdab916f46976230
Gabriel von Platen <gabrielvonplaten@gmail.com>
2019-06-13 18:31:10 +0200

    Parsing a string into an integer

--- Program.cs
+++ Program.cs
@@ -11,26 +11,13 @@ namespace Hello_World
     {
         static void Main(string[] args)
         {
-            // Implicit conversion
-            int num = 1231342;
-            long bigNum = num;
+            string myString = "15";
+            string mySecondString = "13";
+            int num1 = Int32.Parse(myString);
+            int num2 = Int32.Parse(mySecondString);
+            int resultInt = num1 + num2;
 
-            float myFloat = 13.68f;
-            double myNewDouble = myFloat;
-
-            // Explicit conversion
-            // Cast double to int
-            double myDouble = 167.13;
-            int myInt;
-            myInt = (int)myDouble;
-
-            // typeConversion
-            // Convernt to string type
-            string myString = myDouble.ToString(); // "167.13"
-            string myFloatString = myFloat.ToString();
-
-
-            Console.WriteLine(myFloatString);
+            Console.WriteLine(resultInt);
             Console.Read();
             
         }
