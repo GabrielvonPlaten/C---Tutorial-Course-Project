838bd2186e00f77f0544b28f8aecbcaf36726be4
Gabriel von Platen <gabrielvonplaten@gmail.com>
2019-06-13 18:21:10 +0200

    Implicit and Explicit Conversion

--- Program.cs
+++ Program.cs
@@ -6,21 +6,33 @@ using System.Threading.Tasks;
 
 namespace Hello_World
 {
+    // Class names should have the cameCase naming convention
     class Program
     {
-        // Starting point of my program
         static void Main(string[] args)
         {
-            string myName = "Gabriel von Platen";
-            string message = "My name is " + myName;
-            message = message.ToUpper();
+            // Implicit conversion
+            int num = 1231342;
+            long bigNum = num;
 
-            string newMessage = "THIS IS MY NEW MESSAGE";
-            newMessage = newMessage.ToLower();
+            float myFloat = 13.68f;
+            double myNewDouble = myFloat;
 
-            Console.WriteLine(message);
-            Console.WriteLine(newMessage);
+            // Explicit conversion
+            // Cast double to int
+            double myDouble = 167.13;
+            int myInt;
+            myInt = (int)myDouble;
+
+            // typeConversion
+            // Convernt to string type
+            string myString = myDouble.ToString(); // "167.13"
+            string myFloatString = myFloat.ToString();
+
+
+            Console.WriteLine(myFloatString);
             Console.Read();
+            
         }
     }
 }
