4ecae0f1517a70c0f2c38c3b2b4ad725c8a43874
Gabriel von Platen <gabrielvonplaten@gmail.com>
2019-06-13 04:28:58 +0200

    Datatype String and some of its methods

--- Program.cs
+++ Program.cs
@@ -11,23 +11,15 @@ namespace Hello_World
         // Starting point of my program
         static void Main(string[] args)
         {
-            int num1 = 13;
-            int num2 = 24;
-            int num3; // 0
-            int sum = num1 + num2;
-            double d1 = 3.5;
-            double d2 = 1.337;
-            double sumd = d1 / d2;
+            string myName = "Gabriel von Platen";
+            string message = "My name is " + myName;
+            message = message.ToUpper();
 
-            float f1 = 2.4f;
-            // Use casting
-            // int divDandI = num1 / d1; Cannot divide one data type with another which is different (int and double).
+            string newMessage = "THIS IS MY NEW MESSAGE";
+            newMessage = newMessage.ToLower();
 
-            num3 = 15;
-
-            Console.WriteLine("The sum of " + num1 + " and " + num2 + " is " + sum);
-            Console.WriteLine(num1);
-            Console.WriteLine("The sum of the doubles is " + sumd);
+            Console.WriteLine(message);
+            Console.WriteLine(newMessage);
             Console.Read();
         }
     }
