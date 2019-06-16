9a48fb155e75bb3295f1727d2e50fb03fc1ccd70
Gabriel von Platen <gabrielvonplaten@gmail.com>
2019-06-16 22:13:41 +0200

    Registration and Login with basic if else statements

--- Program.cs
+++ Program.cs
@@ -8,27 +8,49 @@ namespace Hello_World
 {
     class Program
     {
+        static string username;
+        static string password;
+
         static void Main(string[] args)
         {
-            Console.Write("Enter your username: ");
-            bool isAdmin = true;
-            bool isRegistered = true;
-            string username = "";
-            username = Console.ReadLine();
-
-            if (isRegistered && username != "" && username.Equals("Admin"))
-            {
-                Console.WriteLine("Welcome back!");
-                Console.WriteLine("Hi there, Admin!");
-                Console.WriteLine("Looged in as {0}", username); 
-            }
-
-            if (isAdmin || isRegistered)
-            {
-                Console.WriteLine("You are logged in");
-            }
-
+            Registration();
+            Login();
             Console.Read();
+        }
+
+        public static void Registration()
+        {
+            Console.WriteLine("Registration.");
+            Console.Write("Enter your username: ");
+            username = Console.ReadLine();
+            Console.Write("Your password: ");
+            password = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Registration completed!");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("------------------------");
+        }
+
+        public static void Login()
+        {
+            Console.WriteLine("Login.");
+            Console.Write("Enter your username: ");
+            if (username == Console.ReadLine())
+            {
+                Console.WriteLine("Enter your password");
+                if (password == Console.ReadLine())
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Logged in!");
+                    Console.ForegroundColor = ConsoleColor.White;
+                } else
+                {
+                    Console.WriteLine("Login failed");
+                }
+            } else
+            {
+                Console.WriteLine("Login failed");
+            }
         }
 
     }
