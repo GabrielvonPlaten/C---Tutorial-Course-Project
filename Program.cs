478c67733ff1f87def0da2e723707e5e3ca0caac
Gabriel von Platen <gabrielvonplaten@gmail.com>
2019-06-18 23:21:10 +0200

    Loops challenge

--- Program.cs
+++ Program.cs
@@ -11,17 +11,42 @@ namespace Hello_World
 
         static void Main(string[] args)
         {
-            for(int counter = 0; counter < 10; counter++)
+            string input = "0";
+            int count = 0;
+            int total = 0;
+            int currentNum = 0;
+
+            Console.WriteLine("Last number was {0}", currentNum);
+            Console.WriteLine("Please enter the next score");
+            Console.WriteLine("Current amount of entries {0}", count);
+            Console.WriteLine("Please enter -1 once you are ready to calculate the average.");
+
+            while (input != "-1")
             {
 
-                if (counter %2 ==0)
+                input = Console.ReadLine();
+                if(input.Equals("-1"))
                 {
-                    Console.WriteLine("Odd number.");
-                    //break;
+                    Console.WriteLine("-----------------------------------------------");
+                    // Calculate average
+                    double average = (double)total / (double)count;
+                    Console.WriteLine("The average score of your students is {0}", average);
+                }
+
+                if (int.TryParse(input, out currentNum) && currentNum > 0 && currentNum < 21)
+                {
+                    total += currentNum;
+                } else
+                {
+                    if (!input.Equals("-1"))
+                    {
+                        Console.WriteLine("Please enter a value between 1 and 20");
+                    }
                     continue;
                 }
-                Console.WriteLine(counter);
-            };
+
+                count++;
+            }
 
             Console.Read();
         }
