2070a5eb0efc26852e2956ac32643843bf33a77e
Gabriel von Platen <gabrielvonplaten@gmail.com>
2019-07-06 23:36:35 +0200

    OOP Basic

---
+++ Human.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World
+{
+    // This class is a blueprint for a datatype
+    class Human
+    {
+        // Member variable
+        public string firstName = "{Placeholder Name}";
+        public string lastName = "{Placeholder Lastname}";
+        public string profession = "{Placeholder Profession}";
+
+        public void IntroduceMyself()
+        {
+            Console.WriteLine("Hi, my name is {0} {1}", firstName, lastName);
+
+        }
+
+        public void Profession()
+        {
+            Console.WriteLine("I am a {0}", profession);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,44 +11,25 @@ namespace Hello_World
 
         static void Main(string[] args)
         {
-            string input = "0";
-            int count = 0;
-            int total = 0;
-            int currentNum = 0;
+            // Create a new class instance
+            Human gabriel = new Human();
+            // Access public class variables
+            gabriel.firstName = "Gabriel";
+            gabriel.lastName = "von Platen";
+            gabriel.profession = "Web Developer";
+            // Call public class methods
+            gabriel.IntroduceMyself();
+            gabriel.Profession();
 
-            Console.WriteLine("Last number was {0}", currentNum);
-            Console.WriteLine("Please enter the next score");
-            Console.WriteLine("Current amount of entries {0}", count);
-            Console.WriteLine("Please enter -1 once you are ready to calculate the average.");
+            Human michael = new Human();
+            michael.firstName = "Michael";
+            michael.lastName = "Smith";
+            michael.IntroduceMyself();
 
-            while (input != "-1")
-            {
+            Human sara = new Human();
+            sara.IntroduceMyself();
 
-                input = Console.ReadLine();
-                if(input.Equals("-1"))
-                {
-                    Console.WriteLine("-----------------------------------------------");
-                    // Calculate average
-                    double average = (double)total / (double)count;
-                    Console.WriteLine("The average score of your students is {0}", average);
-                }
-
-                if (int.TryParse(input, out currentNum) && currentNum > 0 && currentNum < 21)
-                {
-                    total += currentNum;
-                } else
-                {
-                    if (!input.Equals("-1"))
-                    {
-                        Console.WriteLine("Please enter a value between 1 and 20");
-                    }
-                    continue;
-                }
-
-                count++;
-            }
-
-            Console.Read();
+            Console.ReadKey();
         }
     }
 }
